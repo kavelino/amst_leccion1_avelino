ad1a78152000de7d1c22cd68f706498ae7ddc9d5
Estudiante <Estudiante@GGIECTEMWRK221.espol.edu.ec>
2019-11-08 13:15:19 -0500

    App version 1.0

---
+++ app/src/main/java/com/example/amst_l1_avelino_espolservicios/Main2Activity.java
@@ -0,0 +1,21 @@
+package com.example.amst_l1_avelino_espolservicios;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
